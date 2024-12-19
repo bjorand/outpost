c574b3fff385fd037cda6744823830926345bafb
Benjamin Jorand <benjamin.jorand@gmail.com>
2024-12-19 09:17:02 +0100

    new day/light cycle

--- UI/time.gd
+++ UI/time.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	set_text("Time %s" % d.humanTime())
+	#set_text("Time %s" % d.humanTime())
+	set_text("TODO")
