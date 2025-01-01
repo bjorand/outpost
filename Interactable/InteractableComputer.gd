5160664bcbddda8dd077437efd6a5bf3d086ab4a
Benjamin Jorand <benjamin.jorand@gmail.com>
2025-01-01 14:46:09 +0100

    cleanup

--- Interactable/InteractableComputer.gd
+++ Interactable/InteractableComputer.gd
@@ -3,6 +3,8 @@ extends Interactable
 @export var computer: NodePath
 
 @onready var computer_node = get_node(computer)
+@onready var fullscreen_node = computer_node.get_node("CanvasLayer/TextureRect")
+@onready var viewport = computer_node.get_node("SubViewport")
 
 @onready var fullscreen = false
 
@@ -12,7 +14,7 @@ func get_interactions():
 		"actions": [
 			{
 				"text": "Use computer",
-				"fn": interact,
+				"fn": enable_fullscreen,
 			},
 			{
 				"text": "Reset computer",
@@ -30,7 +32,24 @@ func get_interactions():
 func _ready():
 	pass
 	
-func interact():
-	fullscreen = !fullscreen
-	Global.fullscreen_requested.emit(computer_node.viewport.get_texture())
 	
+func enable_fullscreen():
+	fullscreen = !fullscreen
+	#Global.fullscreen_requested.emit(computer_node.viewport.get_texture())
+	fullscreen_node.texture = viewport.get_texture()
+	fullscreen_node.visible = true
+	var s = get_window().size
+	viewport.size = s
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _unhandled_input(event):
+	# Quitte le mode plein écran avec ESC
+	if fullscreen_node.visible:
+		viewport.push_input(event)
+	if fullscreen_node.visible and event is InputEventKey and event.pressed and event.physical_keycode == KEY_ESCAPE:
+		deactivate_fullscreen()
+
+func deactivate_fullscreen():
+	fullscreen_node.visible = false
+	viewport.size = Vector2(512.0, 512.0)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
